5d13a401cb67a8450b312a4f252aae5930bb84e7
atsumu22 <w.atsumu27@gmail.com>
2025-07-05 08:42:51 +0900

    tasksをprojectsにネスト

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,7 @@
 class Project < ApplicationRecord
+  has_many :tasks, dependent: :destroy
   validates :name, presence: true
+
   def self.ransackable_attributes(auth_object = nil)
     %w[name]
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :comments
-  resources :tasks
-  resources :projects
+  resources :projects do
+    resources :tasks
+  end
   root "projects#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
