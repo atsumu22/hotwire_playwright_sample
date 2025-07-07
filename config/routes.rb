200558589009068da3ee548ca6e86f2210a7f2d6
atsumu22 <w.atsumu27@gmail.com>
2025-07-07 16:16:04 +0900

    addedTasks with nested project

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :comments
   resources :projects do
     resources :tasks
+    resource :task_completed, only: [:update]
   end
   root "projects#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
